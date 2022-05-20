41cb130fb57cf8a5ef464ee161371587dadbdeb4
giornotheme <vtang@outlook.fr>
2022-05-20 17:26:58 +0200

    Initial commit

---
+++ src/main/java/fr/uge/jee/springmvc/pokematch/PokeMatchApplication.java
@@ -0,0 +1,12 @@
+package fr.uge.jee.springmvc.pokematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeMatchApplication.class, args);
+    }
+}
